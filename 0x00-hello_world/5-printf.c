7b71e52eef96f2adfeb1e41476245e49b679e214
pauline udie <u.paulineudie@gmail.com>
2023-03-16 17:18:12 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - this is the main function
  *
@@ -9,5 +8,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0):
+	return (0);
 }
